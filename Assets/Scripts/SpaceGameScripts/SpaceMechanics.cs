e817aa8a57746d26b8523dee39ba3a461587972a
Kenneth McLachlan <121731402+KennethMcLachlan@users.noreply.github.com>
2023-12-21 15:41:43 -0800

    Added SFX to Space Game

--- Assets/Scripts/SpaceGameScripts/SpaceMechanics.cs
+++ Assets/Scripts/SpaceGameScripts/SpaceMechanics.cs
@@ -7,19 +7,22 @@ public class SpaceMechanics : MonoBehaviour
 {
     [SerializeField] private GameObject[] _pipeArray;
     private int _activePipeID;
+    private bool[] _isPipeLocked;
 
-    //Rotate Buttons
+    [SerializeField] private GameObject _stars;
+    [SerializeField] private AudioSource _successSFX;
+
     [SerializeField] private Button _rotateLeft;
     [SerializeField] private Button _rotateRight;
 
     void Start()
     {
-        
+        _isPipeLocked = new bool[_pipeArray.Length];
+        _successSFX = gameObject.GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        
     }
     public void AssignPipe(int id)
     {
@@ -28,7 +31,43 @@ public class SpaceMechanics : MonoBehaviour
 
     public void RotateCurrentPipe(int direction)
     {
-        _pipeArray[_activePipeID].transform.Rotate(0, 0, -15 * direction);
+        if (!_isPipeLocked[_activePipeID])
+        {
+            _pipeArray[_activePipeID].transform.Rotate(0, 0, -15 * direction);
+
+            SetRotationAxis();
+        }
+    }
+
+    private void SetRotationAxis()
+    {
+        Vector3 rotation = _pipeArray[_activePipeID].GetComponent<RectTransform>().rotation.eulerAngles;
+
+        if (rotation.z <= 1f)
+        {
+            _pipeArray[_activePipeID].GetComponent<Button>().enabled = false;
+            _isPipeLocked[_activePipeID] = true;
+            _successSFX.Play();
+            StartCoroutine(StarsRoutine());
+        }
+        
+        if (_activePipeID >=  2)
+        {
+            //Enable Round 2
+        }
+
+        if (_activePipeID > 6)
+        {
+            //Enable round 3
+        }
+
+    }
+
+    IEnumerator StarsRoutine()
+    {
+        _stars.SetActive(true);
+        yield return new WaitForSeconds(3.5f);
+        _stars.SetActive(false);
     }
 
 }
